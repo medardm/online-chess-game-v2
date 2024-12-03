2af52cd700cff15f21b8e380523151fb8199a024
Medard Mandane <medardm13@gmail.com>
2024-12-03 23:52:19 +0800

    Add check if a wallet is connected

--- src/components/UI/GameState/GameStateFooter.js
+++ src/components/UI/GameState/GameStateFooter.js
@@ -1,16 +1,26 @@
-import { connectWallet } from "../../../utils/interact";
+import { useEffect, useState } from "react";
+import { connectWallet, getCurrentWalletConnected } from "../../../utils/interact";
 import "./GameStateFooter.scss";
 
 export const GameStateFooter = ({ quitAction, showInventoryAction, sendDrawRequest }) => {
+  const [address, setAddress] = useState('')
+
+  useEffect(() => {
+    getCurrentWalletConnected().then(({ address }) => {
+      setAddress(address)
+    })
+    console.log(address)
+  }, [])
 
   return (
     <div className="GameStateFooter">
       <div className="u-back" onClick={showInventoryAction}></div>
       <div className="u-footer-right">
         <div className="u-connect" onClick={async () => {
-          await connectWallet()
+          const { address } = await connectWallet()
+          setAddress(address)
         }}>
-          <span>Connect</span>
+          <span>{address ? 'Connected' : 'Connect'}</span>
         </div>
         <div className="u-undo" onClick={quitAction}></div>
         <div className="u-draw" onClick={sendDrawRequest}></div>
